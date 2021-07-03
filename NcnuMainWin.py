3cc94b7e1d6538b81b86bae4ade5c2f439e043f7
molaboon <how88889@gmail.com>
2021-07-03 15:49:02 +0800

    先畫在爬

--- NcnuMainWin.py
+++ NcnuMainWin.py
@@ -6,22 +6,27 @@ def createNcnuMainWin():
     win=Tk()
     win.title("暨大官網最新資訊!")
     win.geometry("600x600")
-    main = NcnuMainAPI()
-    Link='''<span style="background-color:#ffcccc"><ul>'''
     textLb=Label(win,text="暨大校園最新資訊",font="Helvetica 20",bg="#ffcccc")
     
-    for anno in main.getAnno():
-        Link+='''<li> <a href={}> {}</a> </li>'''.format(anno.get("link"),anno.get("title") )
-        
-    Link=Link+'''</ul></span>'''
-
     sb=Scrollbar(win)
-    htmlLable=HTMLLabel(win,html=Link)
+    htmlLable=HTMLLabel(win)
     htmlLable.configure(yscrollcommand=sb.set)
     textLb.pack(fill="x")
 
     sb.pack(side=RIGHT,fill="y")
     htmlLable.pack(fill="both",expand=True)
+    win.update()
+    #=====爬=====
+    main = NcnuMainAPI()
+    Link='''<span style="background-color:#ffcccc"><ul>'''
+    for anno in main.getAnno():
+        Link+='''<li> <a href={}> {}</a> </li>'''.format(anno.get("link"),anno.get("title") )
+    Link=Link+'''</ul></span>'''
+    htmlLable.set_html(Link)
+
+
+    
+        
 
     win.mainloop()
 
