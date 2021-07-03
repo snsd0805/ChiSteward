deb93f4f45da057e0e354838963876be4ec56347
Ting-Jun Wang <levi900227@gmail.com>
2021-07-03 17:27:11 +0800

    fix: 新增登入中的暫轉

--- MainTkinter.py
+++ MainTkinter.py
@@ -7,12 +7,27 @@ from Moodle import *
 from NcnuMainWin import *
 from api.moodle import MoodleAPI
 from api.ncnu import NcnuAPI
+import threading
+
+moodle = None
+ncnu = None
 
 win=Tk()
 firstWin(win)
 def checkJson():
-    moodle = MoodleAPI(CONFIG['moodle']['username'], CONFIG['moodle']['password'])
-    ncnu = NcnuAPI(CONFIG['NCNU']['username'], CONFIG['NCNU']['password'])
+    def login():
+        global moodle, ncnu
+        moodle = MoodleAPI(CONFIG['moodle']['username'], CONFIG['moodle']['password'])
+        ncnu = NcnuAPI(CONFIG['NCNU']['username'], CONFIG['NCNU']['password'])
+        tempWin.destroy()
+    
+    tempWin = Tk()
+    Label(tempWin, text="登入中...", font="50").pack()
+    t = threading.Thread(target=login)
+    t.start()
+
+    tempWin.mainloop()
+
     if  moodle.status and ncnu.status:
         mainWin=Tk()
         createMainWin(mainWin,ncnu,moodle)
